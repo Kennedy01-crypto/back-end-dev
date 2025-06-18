d6969695d44776d317b237b41cd46885c3ab5ab7
Jim Kennedy <kennedyomata@gmail.com>
2025-06-18 16:33:03 +0300

    added client side

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,30 @@ app.get("/", (req, res) => {
   res.json("Hello from backend!");
 });
 
+// if there is an ath problem
+// ALTER USER 'Jim'@'localhost' IDENTIFIED WITH mysql_native_password BY '@Main123';
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books ('title', 'desc', 'cover') VALUES (?)";
+  const values = [
+    "title from backend",
+    "desc from backend",
+    "cover from backend",
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been created successfully!");
+  });
+});
 app.listen(8800, () => {
   console.log("Server is running on port 8800");
 });
