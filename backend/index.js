14c8b195af4a663fbdb85096529a7bf85d9983eb
Jim Kennedy <kennedyomata@gmail.com>
2025-06-21 22:51:21 +0300

    added delete function, improved the post function to include price

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mysql from "mysql";
+import cors from "cors";
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 // Get all books
+app.use(cors());
 
 app.get("/books", (req, res) => {
   const q = "SELECT * FROM books";
@@ -29,12 +31,27 @@ app.get("/books", (req, res) => {
 });
 
 app.post("/books", (req, res) => {
-  const q = "INSERT INTO books ('title', 'desc', 'cover') VALUES (?)";
-  const values = [req.body.title, req.body.desc, req.body.cover];
+  const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES (?)";
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
 
   db.query(q, [values], (err, data) => {
     if (err) return res.json(err);
     return res.json("Book has been created successfully!");
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "DELETE FROM books WHERE id = ?";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been deleted successfully!");
   });
 });
 app.listen(8800, () => {
